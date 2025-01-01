a61f1857039cb1090f5f168a277b2208a5a9a778
alfi4000 <149228038+alfi4000@users.noreply.github.com>
2025-01-01 02:47:29 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,14 @@
 <?php
+$CONFIG = array (
   'trusted_domains' =>
   array (
     0 => 'localhost',
     1 => 'studio',
-    2 => '192.168.50.2'
+    2 => '192.168.50.2',
   ),
   'datadirectory' => '/var/www/nextcloud/data',
   'dbtype' => 'mariadb',
-   'version' => '10.6',
+  'version' => '10.6',
   'dbname' => 'nextcloud',
   'dbhost' => 'localhost',
   'dbtableprefix' => 'oc_',
