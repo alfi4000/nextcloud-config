e01f901fe0d43d8627b729c1121d22e702f5c2ad
alfi4000 <149228038+alfi4000@users.noreply.github.com>
2025-01-01 02:46:08 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+  'trusted_domains' =>
+  array (
+    0 => 'localhost',
+    1 => 'studio',
+    2 => '192.168.50.2'
+  ),
+  'datadirectory' => '/var/www/nextcloud/data',
+  'dbtype' => 'mariadb',
+   'version' => '10.6',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'localhost',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'nextcloud',
+  'dbpassword' => 'hack4000',
+  'installed' => true,
+);
